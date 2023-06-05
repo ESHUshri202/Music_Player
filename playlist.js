f6c84e557881496dc2f364186e2d9943c014ef87
Achintya Shrivastava <85828877+ESHUshri202@users.noreply.github.com>
2023-06-06 00:06:32 +0530

    Add files via upload

---
+++ playlist.js
@@ -0,0 +1,26 @@
+let songs =[
+    {
+        name: "Maan Meri Jaan",
+        artist : "King",
+        img: "img1",
+        audio: "Maan Meri Jaan"
+    },
+    {
+        name: "Pyaar Hota Kayi Baar Hai",
+        artist : "Arijit Singh",
+        img: "img2",
+        audio: "Pyaar Hota Kayi Baar Hai"
+    },
+    {
+        name: "Kali Kali Zulfon Ke",
+        artist : "Madhur Sharma",
+        img: "img3",
+        audio: "Kali Kali Zulfon Ke"
+    },
+    {
+        name: "Kali ",
+        artist : "Madhur Sharma",
+        img: "img4",
+        audio: "Kali Kali Zulfon Ke"
+    }
+];
